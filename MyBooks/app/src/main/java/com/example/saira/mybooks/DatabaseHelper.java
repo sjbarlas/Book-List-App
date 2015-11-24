1ceb8f992ac5da175fe35eb391bb511aee725aa4
Saira Barlas <c12809080@mydit.ie>
2015-11-24 13:38:09 +0000

    /

--- MyBooks/app/src/main/java/com/example/saira/mybooks/DatabaseHelper.java
+++ MyBooks/app/src/main/java/com/example/saira/mybooks/DatabaseHelper.java
@@ -1,8 +1,8 @@
-package com.example.saira.mybooks;
-
-import android.database.sqlite.SQLiteOpenHelper;
-
-public class DatabaseHelper extends SQLiteOpenHelper
-{
-
-}
+//package com.example.saira.mybooks;
+//
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class DatabaseHelper extends SQLiteOpenHelper
+//{
+//
+//}
